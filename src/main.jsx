fcefffa6604b884de4673b324d0617c6dd1d4be5
masesk <radon5k@gmail.com>
2023-03-17 16:12:46 -0700

    init

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import ChatClient from './ChatClient'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ChatClient />
+  </React.StrictMode>,
+)
